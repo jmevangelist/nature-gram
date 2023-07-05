bd2384c77606b7702109e286791f12b78a755b2a
Joan Evangelista <jmevangelist@gmail.com>
2023-07-05 08:00:15 +0800

    placeholder interface

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -11,8 +11,9 @@ export class HomeService {
     calls: number = 0
     popular_page: number = 1;
 
+    private timeDiff = (1*5*60*60*1000)
     private now = Date();
-    private createdD1 = new Date(Date.now() - (3*24*60*60*1000))
+    private createdD1 = new Date(Date.now() - this.timeDiff)
     private authService: AuthorizationService = inject(AuthorizationService)
 
     private base: string[][] = [
@@ -20,16 +21,17 @@ export class HomeService {
         ['created_d1',this.createdD1.toString()],
         ['created_d2',this.now],
         ['order_by','votes'],
-        ['per_page','10']]
+        ['per_page','5']]
 
     extraParams():string[][]|undefined{
         let params = Array.from(this.base)
         let taxaPref = this.authService.getTaxaID();
-        params[4][1] =  Math.ceil(Math.random() * 15).toString()
         
         if(this.calls%2){
             params[0][1] = 'false'
             params[3][1] = 'random'
+            params[2][1] = Date();
+            params[4][1] =  Math.ceil(Math.random() * 15).toString()
         }else{
             let page = [[ 'page', this.popular_page.toString() ]]
             params.push(...page)
@@ -50,17 +52,18 @@ export class HomeService {
 
     pushBackDateRange(){
         this.base[2][1] = this.createdD1.toString()
-        this.createdD1 = new Date(Date.parse(this.createdD1.toString()) - (3*24*60*60*1000) )
+        this.timeDiff = this.timeDiff*2
+        this.createdD1 = new Date(Date.parse(this.createdD1.toString()) - this.timeDiff )
         this.base[1][1] = this.createdD1.toString()
         this.popular_page = 1
     }
 
     refresh(){
         this.now = Date();
-        this.createdD1 = new Date(Date.now() - (3*24*60*60*1000))
+        this.timeDiff = (1*5*60*60*1000)
+        this.createdD1 = new Date(Date.now() - this.timeDiff)
         this.base[1][1] = this.createdD1.toString() 
         this.base[2][1] = this.now
         this.Observations.length = 0;
-        this.popular_page = 1
     }
 }
