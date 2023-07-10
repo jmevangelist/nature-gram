8abe0c6f3b9614945827137a07dfdb483cc39f3b
Joan Evangelista <joanevangelista@janet.twiddlebits.tech>
2023-07-10 21:27:18 +0000

    @angular/pwa

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,24 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import appRoutes from './app/app-routes.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
 bootstrapApplication(AppComponent,{
   providers: [
     provideRouter(appRoutes),
-    provideAnimations()
-  ]
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 }).catch(err => console.log(err));
