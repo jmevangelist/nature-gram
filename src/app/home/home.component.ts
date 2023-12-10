edb98f20e8dba17f650756aeef91607d459dc5eb
Joan Evangelista <jmevangelist@gmail.com>
2023-12-10 02:35:17 +0000

    refactor (observations): standardize observations module and apply on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,18 @@
 import { Component, inject, OnDestroy, OnInit, ViewContainerRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observation } from '../inaturalist/inaturalist.interface';
 import { GramComponent } from '../gram/gram.component';
 import { ClarityModule } from '@clr/angular';
 import { HomeService } from './home.service';
-import { Router, RouterLink } from '@angular/router';
-import { Observable, SubscriptionLike } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ClarityIcons, filterGridIcon, bellIcon } from '@cds/core/icon';
 import { HeaderComponent } from '../header/header.component';
 import { ChipsComponent } from '../chips/chips.component';
 import { IconShapeTuple } from '@cds/core/icon/interfaces/icon.interfaces';
 import { NotificationService } from '../notification/notification.service';
 import { IntersectionObserverDirective } from '../shared/intersection-observer.directive';
+import { ObservationsComponent } from '../observations/observations.component';
+import { KeyValue } from '../shared/generic.interface';
 
 @Component({
   selector: 'app-home',
@@ -23,76 +24,57 @@ import { IntersectionObserverDirective } from '../shared/intersection-observer.d
     RouterLink,
     HeaderComponent,
     ChipsComponent,
-    IntersectionObserverDirective
+    IntersectionObserverDirective,
+    ObservationsComponent
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  observations: Observable<Observation[]>;
   homeService: HomeService = inject(HomeService);
   notificationService: NotificationService = inject(NotificationService);
-  loading: Observable<boolean>;
-  end: boolean;
+  q$!: Observable<KeyValue>;
 
   bellIcon: IconShapeTuple;
   notification$: Observable<number>;
   chipGroup: any[];
-  private sub?: SubscriptionLike;
 
-  constructor(private router: Router, private view: ViewContainerRef){
+  constructor(private router: Router, private view: ViewContainerRef, private activatedRoute:ActivatedRoute){
     ClarityIcons.addIcons(filterGridIcon)
-    this.observations = this.homeService.observations$;
-    this.loading = this.homeService.loading$;
-    this.end = false;
 
     this.bellIcon = bellIcon;
     this.notification$ = this.notificationService.notification$;
     this.chipGroup = this.homeService.chipGroup;
 
+    this.q$ = this.activatedRoute.queryParams;
+
   }
 
   ngOnInit(): void {
     this.homeService.refresh();
-    this.moreObservations();
+    let params:KeyValue = this.homeService.getCurrentParams();
+    this.router.navigate(['/home'], { queryParams: params });
   }
 
   ngOnDestroy(): void {
-    this.sub?.unsubscribe();
     this.homeService.saveDefaultFilter();
   }
 
-  trackByItems(index: number, obs: Observation): number { return obs.id; }
-
-  private moreObservations(){
-    this.end = false;
-    this.homeService.loadObservations().then((b)=>{
-      this.end = !b;
-    })
-  }
 
   reload(){
-    document.querySelector('.home')?.scrollIntoView(true)
-    this.end = false;
-    this.homeService.reload().then((b)=>{
-      this.end = !b;
-    });
+    //todo
   }
 
-  selectChip(cG?:any){
-    this.homeService.updateParams(cG)
-    this.homeService.reload().then((b)=>{
-      this.end = !b;
-    })
+  selectChip(cG?:any):void{
+    this.homeService.updateParams(cG);
+    let params:KeyValue = this.homeService.getCurrentParams();
+    this.router.navigate(['/home'], { queryParams: params });
   }
 
   goToNotifications(){
     this.router.navigateByUrl('/notifications')
   }
 
-  intersected(){
-    this.moreObservations();
-  }
 
 }
